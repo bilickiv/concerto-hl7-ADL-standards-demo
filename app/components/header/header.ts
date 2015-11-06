e042b841fce3f4567bae5d8ce55b81d4c609bf5d
Brian Topping <brian@mauswerks.net>
2015-11-05 17:20:23 -0700

    WIP on AccordionGroup subclass.

--- app/components/header/header.ts
+++ app/components/header/header.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef, CORE_DIRECTIVES} from 'angular2/angular2';
-import {Dropdown, DropdownMenu, DropdownToggle, Accordion, AccordionGroup, AccordionHeading} from 'ng2-bootstrap/ng2-bootstrap';
+import {Component, View, ElementRef, CORE_DIRECTIVES} from 'angular2/angular2';
+import {Collapse, Dropdown, DropdownMenu, DropdownToggle, Accordion, AccordionGroup, AccordionHeading, AccordionTransclude} from 'ng2-bootstrap/ng2-bootstrap';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector: 'header-notification',
@@ -23,9 +23,41 @@ export class SidebarSearch {
 }
 
 @Component({
+  selector: 'sb-accordion-group, [sb-accordion-group]',
+  properties: ['templateUrl', 'heading', 'isOpen', 'isDisabled', 'panelClass'],
+  host: {
+    '[class.panel-open]': 'isOpen'
+  }
+})
+@View({
+  template: `
+  <div class="panel" [ng-class]="panelClass">
+    <div class="panel-heading" (click)="toggleOpen($event)">
+      <h4 class="panel-title">
+        <a href tabindex="0" class="accordion-toggle">
+          <span [ng-class]="{'text-muted': isDisabled}"
+            [accordion-transclude]="headingTemplate">{{heading}}</span>
+        </a>
+      </h4>
+    </div>
+    <div class="panel-collapse collapse" [collapse]="!isOpen">
+      <div class="panel-body">
+        <ng-content></ng-content>
+      </div>
+    </div>
+  </div>
+  `,
+  directives: [Collapse, AccordionTransclude, CORE_DIRECTIVES]
+
+})
+export class SbAccordionGroup extends AccordionGroup {
+
+}
+
+@Component({
   selector: 'sidebar',
   templateUrl: './components/header/sidebar.html',
-  directives: [ROUTER_DIRECTIVES, SidebarSearch, Accordion, AccordionGroup, AccordionHeading]
+  directives: [ROUTER_DIRECTIVES, SidebarSearch, Accordion, SbAccordionGroup, AccordionHeading]
 })
 export class Sidebar {
 }
